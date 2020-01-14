3840e76bb4eb81238315dd82dc7bf23b14ce76c6
SONGYEONWON <53226207+SONGYEONWON@users.noreply.github.com>
2020-01-14 21:29:32 +0900

    CONTROLLER / TEST 추가

--- src/main/java/com/demo/springboot/Application.java
+++ src/main/java/com/demo/springboot/Application.java
@@ -1,4 +1,14 @@
 package com.demo.springboot;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication 스프링 부트의 자동설정, 스프링 Bean 읽기와 생성을 모두 자동을 설정한다.
+// @SpringBootApplication 부터 읽어들어가므로 항상 프로젝트 최상단에 위치해야한다.
+@SpringBootApplication
 public class Application {
+    public static void main(String[] args){
+        //내장 WAS 실행!
+        SpringApplication.run(Application.class, args);
+    }
 }
