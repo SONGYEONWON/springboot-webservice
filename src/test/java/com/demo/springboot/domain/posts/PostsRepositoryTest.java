296687ac6a478a8bbd588b68d649463cdd1b12c4
SONGYEONWON <53226207+SONGYEONWON@users.noreply.github.com>
2020-01-23 19:44:53 +0900

    JPA 테스트 / 쿼리 로그 기능 추가

--- src/test/java/com/demo/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/demo/springboot/domain/posts/PostsRepositoryTest.java
@@ -1,7 +1,50 @@
 package com.demo.springboot.domain.posts;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After //junit 에서 단위테스트가 끝날때마다 수행되는 메소드 지정, 여기서는 테스트 후 h2 db 에 데이터를 클리어하기 위한 용도로 사용한다.
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        // 테이블에 post 정보를 insert / update 수행
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("jojoldu@gmail.com")
+                .build());
+
+        //when
+        // post 테이블의 데이터 조회
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        // given 영역에서 입력한 데이터와 저장된 데이터가 동일한지 확인한다.
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
 
 }
