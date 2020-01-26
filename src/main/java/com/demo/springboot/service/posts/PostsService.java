fd4595cbb486e8aae970e06c4243205f819bd7e5
WONSONGYEON <wsy86gkgk@gmail.com>
2020-01-27 00:18:30 +0900

    post controller / dto / service

---
+++ src/main/java/com/demo/springboot/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.demo.springboot.service.posts;
+
+import com.demo.springboot.domain.posts.PostsRepository;
+import com.demo.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
+
